d40be3ecea85b52828a9099e23c86a93d0336ed8
s27467 <s27467@pjwstk.edu.pl>
2024-03-08 15:19:14 +0100

    Print average

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -4,5 +4,18 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World1!");
 Console.WriteLine("Hello, World2!");
 Console.WriteLine("Hello, World3!");
-// ale es
-// sfagasgasga
+
+static double GetAverage(int[] numbers)
+{
+    double result = 0.0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        result += numbers[i];
+    }
+
+    return result / numbers.Length;
+}
+
+int[] numbers = {1,2,3,421,123,12,2,12,12312};
+double average = GetAverage(numbers);
+Console.WriteLine(average);
