8ab2205ed15b5173fe12c581260e2d65e07f1697
s27467 <s27467@pjwstk.edu.pl>
2024-03-08 15:04:47 +0100

    Dodano 3 souty

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World1!");
+Console.WriteLine("Hello, World2!");
+Console.WriteLine("Hello, World3!");
